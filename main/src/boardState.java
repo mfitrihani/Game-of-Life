ea7062067fa64c7b3396dfb3f8c524ffe1946b29
mfitrihani <mfitrihani@gmail.com>
2019-12-02 16:52:23 +0800

    add following method: nextState()-initialize the board for next state calculateState() - determine particular point next state calculateNeighbor() - calculate total alive neighbor

--- main/src/boardState.java
+++ main/src/boardState.java
@@ -14,7 +14,7 @@ public class boardState {
     public void allDeathState(){
         for(int i = 0; i<height;i++){
             for (int j=0;j<width;j++){
-                board[i][j]="-";
+                board[i][j]=" ";
             }
         }
     }
@@ -22,14 +22,13 @@ public class boardState {
     public void allAliveState(){
         for(int i = 0; i<height;i++){
             for (int j=0;j<width;j++){
-                board[i][j]="+";
+                board[i][j]="#";
             }
         }
     }
 
     public void randomState(){
-        Random random = new Random();
-        String[] state = {"+","-"};
+        String[] state = {"#"," "};
 
         for(int i = 0; i<height;i++){
             for (int j=0;j<width;j++){
@@ -39,6 +38,53 @@ public class boardState {
         }
     }
 
+    public void nextState(){
+        for(int i = 0; i<height;i++){
+            for (int j=0;j<width;j++){
+                board[i][j]=calculateState(j,i);
+            }
+        }
+    }
+
+
+    public String calculateState(int xPosition, int yPosition){
+        int totalNeighbor = calculateNeighbor(xPosition,yPosition);
+        String nextState = "?";
+        if(board[yPosition][xPosition].equals("#")){
+            if(totalNeighbor<2){
+                nextState = " ";
+            }
+            else if(totalNeighbor==2||totalNeighbor==3){
+                nextState = "#";
+            }
+            else if(totalNeighbor>3){
+                nextState = " ";
+            }
+        }
+        else if (board[xPosition][yPosition].equals(" ")&&totalNeighbor==3){
+            nextState = "#";
+        }
+
+
+        return nextState;
+    }
+
+    public int calculateNeighbor(int x_position,int y_position){
+        int totalNeighbor=0;
+
+        for (int x = -1;x<=1;x++){
+            for (int y = -1;y<=1;y++){
+                if ((x_position+x<0)||(y_position+y<0)||(x_position+x>width-1)||(y_position+y>height-1)||(x==0&&y==0)){
+                    continue;
+                 }
+                if (board[y_position+y][x_position+x].equals("#")){
+                    totalNeighbor++;
+                }
+            }
+        }
+        return totalNeighbor;
+    }
+
     public void printBoard(){
         for(int i = 0; i<height;i++){
             for (int j=0;j<width;j++){
@@ -46,6 +92,7 @@ public class boardState {
             }
             System.out.println();
         }
+        System.out.println("------------------------------");
     }
 
     public String[][] getBoard() {
--- main/src/main.java
+++ main/src/main.java
@@ -2,8 +2,17 @@
 public class main {
 
     public static void main(String args[]){
-        boardState test = new boardState(5,5);
-        test.randomState();
+        boardState test = new boardState(6,20);
+        test.allAliveState();
         test.printBoard();
+        test.nextState();
+        test.printBoard();
+
+        for (int i =0; i<=5; i++){
+            for (int j =0 ;j<20;j++){
+                System.out.print(test.calculateNeighbor(j,i));
+            }
+            System.out.println();
+        }
     }
 }
